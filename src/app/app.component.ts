57af3c0a133e4fa868872cb786db28de55867656
DracFiendMG <sreeramrdy@gmail.com>
2023-11-26 13:44:40 +0530

    added projects component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, 
+              RouterOutlet, 
+              HeaderComponent, 
+              ProjectsComponent]
 })
 export class AppComponent {
   title = 'portfolio-angular';
