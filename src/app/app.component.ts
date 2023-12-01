c8a49f7a6aaee25e0afc32d4c6e9b12c839706c9
DracFiendMG <sreeramrdy@gmail.com>
2023-12-01 16:59:57 +0530

    used timeline template and added it to the timeline component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { TimelineComponent } from "./timeline/timeline.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, 
-              RouterOutlet, 
-              HeaderComponent, 
-              ProjectsComponent]
+    imports: [CommonModule,
+        RouterOutlet,
+        HeaderComponent,
+        ProjectsComponent, TimelineComponent]
 })
 export class AppComponent {
   title = 'portfolio-angular';
