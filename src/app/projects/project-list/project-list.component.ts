6e1ebf195257793301dac343dc74a12b9b756348
DracFiendMG <sreeramrdy@gmail.com>
2023-12-02 17:10:48 +0530

    added real time data for projects

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectItemComponent } from "./project-item/project-item.component";
 import { Project } from '../project.model';
+// import * as projectData from 'assets/projectData.json'
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @Component({
     selector: 'app-project-list',
@@ -9,15 +11,26 @@ import { Project } from '../project.model';
     templateUrl: './project-list.component.html',
     styleUrl: './project-list.component.css',
     imports: [CommonModule, 
-              ProjectItemComponent]
+              ProjectItemComponent,
+              HttpClientModule]
 })
 export class ProjectListComponent {
-  projects: Project[] = [
-    new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
-    new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
-    new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
-    new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
-    new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
-    new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator')
-  ]
+  projects!: Project[];
+  url: string = 'assets/projectData.json'
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Project[]>(this.url).subscribe(res => {
+      this.projects = res
+    })
+  }
+  // projects: Project[] = [
+  //   new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
+  //   new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
+  //   new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
+  //   new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
+  //   new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator'),
+  //   new Project('A Test Project','This is a simple test','https://upload.wikimedia.org/wikipedia/en/thumb/1/1e/IchigoKurosakiBleach.jpg/220px-IchigoKurosakiBleach.jpg','https://github.com/DracFiendMG/meme-generator')
+  // ]
 }
