e9d69f4393d92c8fb9a6f32257d1576b83039856
DracFiendMG <sreeramrdy@gmail.com>
2024-06-30 17:24:15 +0530

    Developed the first half of the homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,21 @@ import { TimelineComponent } from "./timeline/timeline.component";
 import { PagesComponent } from "./pages/pages.component";
 import { FooterComponent } from "./footer/footer.component";
 import { ProjectsService } from './projects/projects.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+    providers: [ProjectsService],
     imports: [CommonModule,
         RouterOutlet,
         HeaderComponent,
-        ProjectsComponent, 
-        TimelineComponent, 
-        PagesComponent, 
-        FooterComponent],
-    providers: [ProjectsService]
+        ProjectsComponent,
+        TimelineComponent,
+        PagesComponent,
+        FooterComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'portfolio-angular';
---
+++ src/app/home/home.service.ts
@@ -0,0 +1,11 @@
+import { DisplayImage } from "./display-image.model";
+
+export class HomeService {
+    displayImages: DisplayImage[] = [
+        {
+            name: "profile-image",
+            description: "my profile image",
+            imagePath: "https://www.themarysue.com/wp-content/uploads/2022/02/bleach-anime-big-three-is-iconic.jpg?w=1200"
+        }
+    ]
+}
