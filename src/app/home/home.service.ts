05543d2cdf2cddd75d01cb92da3abad65d3ef610
DracFiendMG <sreeramrdy@gmail.com>
2024-07-24 21:49:10 +0530

    Resolving image not loading issue

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -6,52 +6,52 @@ export class HomeService {
             name: "profile-image",
             description: "my profile image",
             // imagePath: "https://www.themarysue.com/wp-content/uploads/2022/02/bleach-anime-big-three-is-iconic.jpg?w=1200"
-            imagePath: "../assets/profile-image.png"
+            imagePath: "./assets/profile-image.png"
         },
         {
             name: "figma-icon",
             description: "my profile image",
-            imagePath: "../assets/figma-icon.png"
+            imagePath: "./assets/figma-icon.png"
         },
         {
             name: "angular-icon",
             description: "my profile image",
-            imagePath: "../assets/angular-icon.png"
+            imagePath: "./assets/angular-icon.png"
         },
         {
             name: "java-icon",
             description: "my profile image",
-            imagePath: "../assets/java-icon.png"
+            imagePath: "./assets/java-icon.png"
         },
         {
             name: "springboot-icon",
             description: "my profile image",
-            imagePath: "../assets/springboot-icon.png"
+            imagePath: "./assets/springboot-icon.png"
         },
         {
             name: "typescript-icon",
             description: "my profile image",
-            imagePath: "../assets/typescript-icon.png"
+            imagePath: "./assets/typescript-icon.png"
         },
         {
             name: "html-icon",
             description: "my profile image",
-            imagePath: "../assets/html-icon.webp"
+            imagePath: "./assets/html-icon.webp"
         },
         {
             name: "css-icon",
             description: "my profile image",
-            imagePath: "../assets/css-icon.png"
+            imagePath: "./assets/css-icon.png"
         },
         {
             name: "mulesoft-icon",
             description: "my profile image",
-            imagePath: "../assets/mulesoft-icon.webp"
+            imagePath: "./assets/mulesoft-icon.webp"
         },
         {
             name: "sql-icon",
             description: "my profile image",
-            imagePath: "../assets/sql-icon.png"
+            imagePath: "./assets/sql-icon.png"
         },
     ]
 }
